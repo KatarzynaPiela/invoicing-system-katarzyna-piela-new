57e523a41f279170103a11ee19c5144406f00d2a
Marek Wisniewski <a@a.pl>
2024-04-12 15:58:57 +0200

    Fixes during consultation

--- src/main/java/pl/futurecollars/invoicing/utils/JsonMappingException.java
+++ src/main/java/pl/futurecollars/invoicing/utils/JsonMappingException.java
@@ -1,7 +1,9 @@
 package pl.futurecollars.invoicing.utils;
 
 public class JsonMappingException extends RuntimeException {
+
   public JsonMappingException(String message, Throwable cause) {
     super(message, cause);
   }
+
 }
