81900196ad037c961eac4d9103c38395b90a98ec
KatarzynaPiela <155573443+KatarzynaPiela@users.noreply.github.com>
2024-04-29 18:33:18 +0200

    Lesson7 (#11)
    * lesson6
    
    * lesson7
    
    * lesson7
    
    * lesson7

---
+++ src/main/java/pl/futurecollars/invoicing/db/DatabaseConfiguration.java
@@ -0,0 +1,50 @@
+package pl.futurecollars.invoicing.db;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pl.futurecollars.invoicing.db.file.FileBasedDatabase;
+import pl.futurecollars.invoicing.db.file.IdService;
+import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
+import pl.futurecollars.invoicing.utils.FilesService;
+import pl.futurecollars.invoicing.utils.JsonService;
+
+@Slf4j
+@Configuration
+public class DatabaseConfiguration {
+
+  @Bean
+  public IdService idService(FilesService filesService,
+                             String databaseDirectory,
+                             String idFile
+  ) throws IOException {
+    Path idFilePath = Files.createTempFile(databaseDirectory, idFile);
+    return new IdService(idFilePath, filesService);
+  }
+
+  @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "file")
+  @Bean
+  public Database fileBasedDatabase(IdService idService,
+                                    FilesService filesService,
+                                    JsonService jsonService,
+                                    String databaseDirectory,
+                                    String invoiceFile) throws IOException {
+    log.trace("Used file database - trace");
+    log.debug("Used file database - debug");
+    log.info("Used file database - info");
+    log.warn("Used file database - warn");
+    log.error("Used file database - error");
+    Path databaseFilePath = Files.createTempFile(databaseDirectory, invoiceFile);
+    return new FileBasedDatabase(filesService, jsonService, idService, databaseFilePath);
+  }
+
+  @Bean
+  public Database inMemoryDatabase() {
+    log.info("Used InMemoryDatabase");
+    return new InMemoryDatabase();
+  }
+}
