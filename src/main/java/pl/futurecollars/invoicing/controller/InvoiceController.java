7e470b581fc417be215b0f19341649c35785fc04
KatarzynaPiela <Katarzyna.koterba@wp.pl>
2024-05-05 12:57:10 +0200

    lesson8

--- src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -1,6 +1,7 @@
 package pl.futurecollars.invoicing.controller;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,43 +12,61 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.InvoiceService;
 
+@Slf4j
 @RestController
 @RequestMapping("invoices")
 public class InvoiceController {
+  public class InvoiceControllers implements InvoiceApi {
 
-  private InvoiceService invoiceService = new InvoiceService(new InMemoryDatabase());
+    private final InvoiceService invoiceService;
 
-  @Autowired
-  public InvoiceController(InvoiceService invoiceService) {
-    this.invoiceService = invoiceService;
-  }
+    @Autowired
+    public InvoiceControllers(InvoiceService invoiceService) {
+      this.invoiceService = invoiceService;
+    }
 
-  @PostMapping
-  public int add(@RequestBody Invoice invoice) {
-    return invoiceService.save(invoice);
-  }
+    @GetMapping(produces = {"application/json;charset=UTF-8"})
+    @Override
+    public List<Invoice> getAll() {
+      return invoiceService.getAll();
+    }
 
-  @GetMapping
-  public List<Invoice> getAll() {
-    return invoiceService.getAll();
-  }
+    @PostMapping
+    @Override
+    public int add(@RequestBody Invoice invoice) {
+      log.info("Używam postMapping update - info");
+      return invoiceService.save(invoice);
+    }
 
-  @GetMapping("/{id}")
-  public ResponseEntity<Invoice> getById(@PathVariable int id) {
-    return invoiceService.getById(id).map(invoice -> ResponseEntity.ok().body(invoice)).orElse(ResponseEntity.notFound().build());
-  }
+    @GetMapping(value = "/{id}", produces = {"application/json;charset=UTF-8"})
+    @Override
+    public ResponseEntity<Invoice> getById(@PathVariable int id) {
+      log.info("Używam getMapping update - info");
+      return invoiceService.getById(id)
+          .map(invoice -> ResponseEntity.ok().body(invoice))
+          .orElse(ResponseEntity.notFound().build());
+    }
 
-  @PutMapping("/{id}")
-  public ResponseEntity<?> update(@PathVariable int id, @RequestBody Invoice invoice) {
-    return invoiceService.update(id, invoice).map(deletedInvoise -> ResponseEntity.noContent().build()).orElse(ResponseEntity.notFound().build());
-  }
+    @DeleteMapping("/{id}")
+    @Override
+    public ResponseEntity<?> deleteById(@PathVariable int id) {
+      log.info("Używam deleteMapping update - info");
+      return invoiceService.delete(id)
+          .map(name -> ResponseEntity.noContent().build())
+          .orElse(ResponseEntity.notFound().build());
+    }
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<?> deleteById(@PathVariable int id) {
-    return invoiceService.delete(id).map(deletedInvoise -> ResponseEntity.noContent().build()).orElse(ResponseEntity.notFound().build());
+    @PutMapping("/{id}")
+    @Override
+    public ResponseEntity<?> update(@PathVariable int id, @RequestBody Invoice invoice) {
+      log.info("Używam putMapping update - info");
+      return invoiceService.update(id, invoice)
+          .map(name -> ResponseEntity.noContent().build())
+          .orElse(ResponseEntity.notFound().build());
+    }
   }
 }
+
